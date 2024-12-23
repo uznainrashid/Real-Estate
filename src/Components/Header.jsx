46ea4ed54a01a24417471a8a6f866dd4cfe0f869
uznainrashid <147235060+uznainrashid@users.noreply.github.com>
2024-12-23 20:37:22 +0500

    Header Change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       id="Header"
     >
       <Navbar />
-      <div className="container mt-40 text-center mx-auto px-6 py-4 md:px-20 lg:px-32 text-white" >
+      <div className="container mt-48 text-center mx-auto px-6 py-4 md:px-20 lg:px-32 text-white" >
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold">Explore Home That Fit Your Dreams</h2>
         <div className="space-x-6 mt-16">
           <a href="#Projects" className="border border-white px-8 py-3 rounded hover">Projects</a>
